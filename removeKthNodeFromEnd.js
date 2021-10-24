73773d3267ac96f5f03280dcecd07a57c16669a6
Caleb D <46833575+gcdco@users.noreply.github.com>
2021-10-24 13:55:22 -0600

    Create removeKthNodeFromEnd.js

---
+++ removeKthNodeFromEnd.js
@@ -0,0 +1,31 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeKthNodeFromEnd(head, k) {
+	let first = head;
+	let second = head;
+	let counter = 1;
+	while(counter <= k){
+		second = second.next;
+		counter++;
+	}
+	if(second === null){
+		head.value = head.next.value;
+		head.next = head.next.next;
+		return;
+	}
+	while(second.next !== null){
+		second = second.next;
+		first = first.next;
+	}
+	first.next = first.next.next
+}
+
+// Do not edit the lines below.
+exports.LinkedList = LinkedList;
+exports.removeKthNodeFromEnd = removeKthNodeFromEnd;
